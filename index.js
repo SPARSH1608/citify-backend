c159916140bb74e8a53950aab9337324f337017b
Sparsh Goel <sparshgeolk.com>
2024-01-27 14:15:51 +0530

    solved cors

--- index.js
+++ index.js
@@ -13,27 +13,14 @@ const multer = require('multer');
 const uploadMiddlerWare = multer({ dest: 'uploads/' });
 const fs = require('fs');
 const Post = require('./models/Post');
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', ' http://localhost:5173');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Private-Network', true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader('Access-Control-Max-Age', 7200);
 
-  next();
-});
 app.use(
   cors({
     credentials: true,
-    origin: ' http://localhost:5173',
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:
+      'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
   })
 );
 app.options('*', (req, res) => {
@@ -49,17 +36,14 @@ app.options('*', (req, res) => {
     console.log('fail');
   }
 });
+
 app.use(express.json()); // to get req bbdoy
 app.use(cookieParser()); //to parse cookie
 app.use('/uploads', express.static(__dirname + '/uploads'));
 const salt = bcrypt.genSaltSync(10);
-app.options('*', cors());
+
 mongoose.connect(process.env.MONGO_URL);
-const corsOptions = {
-  origin: ' http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-};
-app.use(cors(corsOptions));
+
 // const start = async () => {
 //   try {
 //     await Connectdb(process.env.MONGO_URI);

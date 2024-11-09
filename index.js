004ba4136e42d5541ebc1e97af80643d3816cd97
Sparsh <sparshgoelk@gmail.com>
2024-11-09 22:17:28 +0530

    added cors files updates

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.SECRET_KEY;
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-const uploadMiddlerWare = multer({ dest: 'uploads/' });
+const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 const Post = require('./models/Post');
 const allowMethods = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
@@ -61,7 +61,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 const salt = bcrypt.genSaltSync(10);
 
 mongoose.connect(
-  'mongodb+srv://sparshgoelk:HDQeNPC0ZFvEWnep@cluster0.kkogtln.mongodb.net/citify?retryWrites=true&w=majority'
+  'mongodb+srv://sparshgoelk:8dpl7GIr9wAGvtfO@cluster0.kkogtln.mongodb.net/citify?retryWrites=true&w=majority'
 );
 
 app.listen(process.env.PORT, (PORT) => {
@@ -70,170 +70,111 @@ app.listen(process.env.PORT, (PORT) => {
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-
-  // res.json({ requestData: { username, password } });
-
   try {
     const userDoc = await User.create({
       username,
       password: bcrypt.hashSync(password, salt),
     });
-
-    res.status(200).json(userDoc);
-  } catch (error) {
-    res.status(400).json(error);
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
   }
 });
+
 app.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const userDoc = await User.findOne({ username: username });
-
-    if (!userDoc) {
-      return res.status(400).json({ message: 'User not found' });
-    }
-
-    const passOK = bcrypt.compareSync(password, userDoc.password);
-
-    if (passOK) {
-      // Correct password
-      const token = jwt.sign({ username, id: userDoc._id }, secret, {
-        expiresIn: '1h',
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
       });
-
-      res
-        .cookie('token', token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: 'None',
-        })
-        .json({ id: userDoc._id, username: userDoc.username });
-    } else {
-      res.status(400).json({ message: 'Wrong credentials' });
-    }
-  } catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
   }
 });
+
 app.get('/profile', (req, res) => {
   const { token } = req.cookies;
-  if (token) {
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
-        console.error('Error verifying token:', err);
-        return res.status(401).json({ error: 'Invalid token' });
-      }
-
-      res.json({ info: decoded });
-    });
-  }
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
 });
 
 app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddlerWare.single('file'), async (req, res) => {
-  try {
-    // Process the request
-    const { originalname, path } = req.file;
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path + '.' + ext;
+  fs.renameSync(path, newPath);
 
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
-    fs.renameSync(path, path + '.' + ext);
-
-    const { token } = req.cookies;
-
-    jwt.verify(token, secret, {}, async (err, info) => {
-      if (err) {
-        throw new Error({ message: 'invalid token' });
-      }
-      const { title, summary, content } = req.body;
-      console.log(title, summary, content, newPath, info.id);
-      const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
-        author: info.id,
-      });
-      console.log(postDoc);
-      res.json(postDoc);
-    });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: 'Internal Server Error while creating post' });
+    newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
   }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
 });
 
 app.get('/post', async (req, res) => {
-  try {
-    const { sortOption } = req.query;
-
-    let sortCriteria = { createdAt: -1 }; // Default sorting by date (latest to old)
-
-    if (sortOption === 'dateAsc') {
-      sortCriteria = { createdAt: 1 }; // Sorting by date ascending
-    } else if (sortOption === 'alphabetical') {
-      sortCriteria = { title: 1 }; // Sorting alphabetically by post title
-    }
-
-    const posts = await Post.find()
+  res.json(
+    await Post.find()
       .populate('author', ['username'])
-      .sort(sortCriteria)
-      .limit(20);
-
-    res.json(posts);
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
 });
 
 app.get('/post/:id', async (req, res) => {
   const { id } = req.params;
-  const post = await Post.findById(id).populate('author', ['username']);
-  res.json(post);
-});
-
-app.put('/postt/:id', uploadMiddlerWare.single('file'), async (req, res) => {
-  try {
-    let newPath = null;
-    if (req.file) {
-      const { originalname, path } = req.file;
-
-      const parts = originalname.split('.');
-      const ext = parts[parts.length - 1];
-      const newPath = path + '.' + ext;
-      fs.renameSync(path, path + '.' + ext);
-    }
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, async (err, info) => {
-      if (err) {
-        throw new Error({ message: 'invalid token' });
-      }
-      const { id, title, summary, content, support } = req.body;
-      const postDoc = await Post.findById(id);
-      if (!postDoc) {
-        return res.status(404).json({ error: 'Post not found' });
-      }
-
-      await postDoc.updateOne({
-        title,
-        summary,
-        content,
-        cover: newPath ? newPath : postDoc.cover,
-        support,
-      });
-      console.log(postDoc);
-      res.json(postDoc);
-    });
-  } catch (error) {
-    console.error('Error while updating post:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
 });
